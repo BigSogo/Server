5262a7192614e568ed413de2d0390f21a8ac8f49
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-16 10:47:10 +0900

    Feat :: 회원가입 기능

--- domain/user/user_router.py
+++ domain/user/user_router.py
@@ -1,22 +1,25 @@
 from fastapi import APIRouter
 
+from passlib.context import CryptContext
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
+
 from globals.db import session
-from domain.user.dto import register_dto
-from domain.user.model import user_table
+from domain.user.dto.register_dto import Register
+from domain.user.model.user_table import User
 
 router = APIRouter()
 
 @router.post("/register")
-async def register(dto: register_dto.Register):
-    user = user_table()
+async def register(dto: Register):
+    user = User(
+        email = dto.email,
+        username = dto.username,
+        password = bcrypt_context.hash(dto.password),
+        major = dto.major
+    )
 
-    user.email = dto.email
-    user.name = dto.name
-    user.username = dto.username
-    user.password = dto.password
-    user.major = dto.major
+    session.add(user)
+    session.commit()
 
-    await session.add(user)
-    await session.commit()
-
-    return f"{dto.name} created..."
+    return f"{dto.username} created..."

efb9b81bbc841138edcf40e0f33d53192c093ac0
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-16 10:49:50 +0900

    Refactor :: 패키지 구조 변경

--- domain/user/user_router.py
+++ domain/user/user_router.py
@@ -1,15 +1,19 @@
+# 기본 모듈
 from fastapi import APIRouter
 
+# BCrypt
 from passlib.context import CryptContext
-
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
 
+# 관련 모듈
 from globals.db import session
-from domain.user.dto.register_dto import Register
-from domain.user.model.user_table import User
+from domain.user.user_dto import Register
+from domain.user.user_table import User
 
+# 라우터 설정
 router = APIRouter()
 
+# 회원가입
 @router.post("/register")
 async def register(dto: Register):
     user = User(

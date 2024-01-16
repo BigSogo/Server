aa78369827af866177b103a057780c602e1955fa
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-16 13:46:41 +0900

    Feat :: JWT 발급

--- domain/user/user_dto.py
+++ domain/user/user_dto.py
@@ -1,7 +1,11 @@
-from pydantic import BaseModel;
+from pydantic import BaseModel
 
 class Register(BaseModel) :
     email : str
     username : str
     password : str
     major : str
+
+class Login(BaseModel) :
+    email : str
+    password : str

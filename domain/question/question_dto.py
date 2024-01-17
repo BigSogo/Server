bad0b97d300e0916fe53aff90db9ffa751cb7558
gwon11225 <gwon11225@gmail.com>
2024-01-17 15:59:12 +0900

    Refactor :: Optional 제거

--- domain/question/question_dto.py
+++ domain/question/question_dto.py
@@ -10,10 +10,10 @@ class CreateQuestion(BaseModel):
     senior_id: Optional[int] = None
 
 class GetQuestion(BaseModel):
-    id: Optional[int] = None
-    title: Optional[str] = None
-    content: Optional[str] = None
-    date: Optional[datetime] = None
-    writer: Optional[UserResponse] = None
+    id: int = None
+    title: str = None
+    content: str = None
+    date: datetime = None
+    writer: UserResponse = None
     senior: Optional[UserResponse] = None
     comments: list[Optional[CommentDto]] = None

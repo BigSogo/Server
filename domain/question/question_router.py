d540b6a71ce53e3c9f244362e2594407e8593631
gwon11225 <gwon11225@gmail.com>
2024-01-17 16:50:33 +0900

    Feat :: 질문 불러오기 완성

--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -3,10 +3,11 @@ from http import HTTPStatus
 from sqlalchemy.orm import Session
 
 from domain.question.question_table import Question
-from domain.question.question_dto import CreateQuestion, GetQuestion
-from domain.user.user_dto import UserResponse
 from domain.user.user_table import User
+
+from domain.question.question_dto import CreateQuestion, GetQuestion, GetQuestionList
 from domain.question.comment_dto import CommentDto
+from domain.user.user_dto import UserResponse
 
 from globals.base_response import BaseResponse
 from globals.db import get_db
@@ -35,7 +36,7 @@ async def create_question(request: CreateQuestion, db: Session = Depends(get_db)
 
     return BaseResponse(code=HTTPStatus.OK, message="생성완료")
 
-@router.get("/question/{question_id}", response_model=BaseResponse[GetQuestion])
+@router.get("/question", response_model=BaseResponse[GetQuestion])
 async def get_question(question_id:int, db: Session = Depends(get_db)):
     question = db.query(Question).get(question_id)
     if question == None:
@@ -61,7 +62,21 @@ async def get_question(question_id:int, db: Session = Depends(get_db)):
             comments=comments
         )
         return BaseResponse(code=200, message="조회 성공", data=response)
+
+@router.get("/question/list", response_model=BaseResponse[list[GetQuestionList]])
+async def get_question_list(db: Session = Depends(get_db)):
+    questions = db.query(Question).all()
     
+    question_list = [GetQuestionList(
+        id=question.id,
+        title=question.title,
+        date=question.date,
+        writer=__create_user_response(question.writer),
+        senior=None if question.senior == None else __create_user_response(question.senior)
+    ) for question in questions]
+
+    return BaseResponse(code=200, message="조회 완료", data=question_list)
+
 def __create_user_response(user: User):
     return UserResponse(
         id=user.id,
@@ -70,7 +85,3 @@ def __create_user_response(user: User):
         description=user.description,
         major=user.major.split('|')
     )
-
-async def get_question_list():
-    pass
-

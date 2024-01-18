7ba5622f0dd0f47e8b4b76d06d043de1f0605121
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-18 12:45:25 +0900

    Feat :: 프로필 사진 수정 기능

--- domain/profile/router.py
+++ domain/profile/router.py
@@ -1,5 +1,5 @@
 # 기본 모듈
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, UploadFile, File, HTTPException
 from sqlalchemy.orm import Session
 from typing import Optional
 
@@ -9,13 +9,15 @@ from domain.profile.table import Profile
 from globals.base_response import BaseResponse
 from domain.profile.dto import ProfileResponse
 from domain.user.dto import create_user_response
+from domain.user.table import User
+from globals.jwt import get_current_user
 
 router = APIRouter()
 
 # 포트폴리오 가져오기
 @router.get("", response_model=BaseResponse)
 async def get_profile(query: Optional[str] = None, db: Session = Depends(get_db)) :
-    results = db.query(Profile).filter(Profile.major.like(f"%{query}%"))
+    results = db.query(User).filter(Profile.major.like(f"%{query}%"))
 
     datas = [ProfileResponse(
         id=profile.id,
@@ -30,3 +32,21 @@ async def get_profile(query: Optional[str] = None, db: Session = Depends(get_db)
         message = "프로필 검색 성공",
         data = datas
     )
+
+@router.patch("/update-image", response_model=BaseResponse)
+async def update_image(file: UploadFile = File(...), db: Session = Depends(get_db), current_user: dict = Depends(get_current_user)) :
+    image = await file.read()
+
+    user = db.query(User).filter(User.id == current_user["id"]).first()
+
+    if user :
+        user.profile_img = image
+        db.add(user)
+        db.commit()
+        return BaseResponse(
+            code = 200,
+            message = "이미지 업로드 성공",
+            data = None
+        )
+    else :
+        raise HTTPException(400, "잘못된 정보")
--- domain/user/table.py
+++ domain/user/table.py
@@ -1,15 +1,15 @@
-from sqlalchemy import Column, Integer, String, TEXT
+from sqlalchemy import Column, Integer, String, LargeBinary
 from sqlalchemy.orm import relationship
 from globals.db import Base
 
 class User(Base):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True, index=True)
+    profile_img = Column(LargeBinary)
     email = Column(String(50))
     username = Column(String(10))
     password = Column(String(100))
     description = Column(String(100))
-    major = Column(String(100))
 
     profile = relationship("Profile", back_populates="user", uselist=False)
     questions_writer = relationship("Question", back_populates="writer", foreign_keys="[Question.writer_id]")

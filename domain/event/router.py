17e823a0da88ecb3e7dbfcaf1a5bc9dfb2eed866
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-18 14:16:25 +0900

    Feat :: 컨퍼런스 조회 & 생성

---
+++ domain/event/router.py
@@ -0,0 +1,30 @@
+# 기본 모듈
+from fastapi import APIRouter, UploadFile, File, Depends
+from sqlalchemy.orm import Session
+import io
+
+# 관련 모듈
+from globals.jwt import get_db
+from domain.event.table import Event
+from globals.base_response import BaseResponse
+
+router = APIRouter()
+
+@router.get("")
+async def get_event(db: Session = Depends(get_db)) :
+    return db.query(Event).all()
+
+@router.post("", response_model=BaseResponse)
+async def upload_event(file: UploadFile = File(...), db: Session = Depends(get_db)) :
+    image = await file.read()
+
+    db_image = Event(event_img=image)
+    db.add(db_image)
+    db.commit()
+
+    return BaseResponse(
+        code = 200,
+        message = "이미지 업로드 성공",
+        data = None
+    )
+

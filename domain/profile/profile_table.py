05bc0e9498dff2862e4de094f5e9e83547fba3c1
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-18 09:51:59 +0900

    Feat :: Profile 테이블 생성

--- domain/profile/profile_table.py
+++ domain/profile/profile_table.py
@@ -1,10 +1,17 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, Text, String
+from sqlalchemy.orm import relationship
 from globals.db import Base
 
-class Comment(Base):
+class Profile(Base):
     __tablename__ = "profile"
     __table_args__ = {
         'mysql_engine':'InnoDB'
     }
 
-    id = Column(Integer, primary_key=true)
+    id = Column(Integer, primary_key=True, index=True)
+    user = relationship("User", back_populates="profile", uselist=False)
+    major = Column(Text)
+    subject = Column(String(100))
+    content = Column(Text)
+    portfolio_url = Column(String(100))
+    tag = Column(String(100))
